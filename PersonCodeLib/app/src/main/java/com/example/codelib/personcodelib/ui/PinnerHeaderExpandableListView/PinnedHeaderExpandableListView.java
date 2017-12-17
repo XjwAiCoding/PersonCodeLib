1bddccac7d0afd000596360f5c90a8cdcf7806c6
xujiawei <627673188@qq.com>
2017-12-17 21:24:45 +0800

    增加固定头部ListView

---
+++ PersonCodeLib/app/src/main/java/com/example/codelib/personcodelib/ui/PinnerHeaderExpandableListView/PinnedHeaderExpandableListView.java
@@ -0,0 +1,277 @@
+/**
+The MIT License (MIT)
+
+Copyright (c) 2014 singwhatiwanna
+https://github.com/singwhatiwanna
+http://blog.csdn.net/singwhatiwanna
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package com.example.codelib.personcodelib.ui.PinnerHeaderExpandableListView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ExpandableListView;
+
+public class PinnedHeaderExpandableListView extends ExpandableListView implements OnScrollListener {
+    private static final String TAG = "PinnedHeaderExpandableListView";
+    private static final boolean DEBUG = true;
+
+    public interface OnHeaderUpdateListener {
+        /**
+         * 返回一个view对象即可
+         * 注意：view必须要有LayoutParams
+         */
+        public View getPinnedHeader();
+
+        public void updatePinnedHeader(View headerView, int firstVisibleGroupPos);
+    }
+
+    private View mHeaderView;
+    private int mHeaderWidth;
+    private int mHeaderHeight;
+
+    private View mTouchTarget;
+
+    private OnScrollListener mScrollListener;
+    private OnHeaderUpdateListener mHeaderUpdateListener;
+
+    private boolean mActionDownHappened = false;
+    protected boolean mIsHeaderGroupClickable = true;
+
+
+    public PinnedHeaderExpandableListView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public PinnedHeaderExpandableListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public PinnedHeaderExpandableListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
+
+    private void initView() {
+        setFadingEdgeLength(0);
+        setOnScrollListener(this);
+    }
+
+    @Override
+    public void setOnScrollListener(OnScrollListener listener) {
+        if (listener != this) {
+            mScrollListener = listener;
+        } else {
+            mScrollListener = null;
+        }
+        super.setOnScrollListener(this);
+    }
+
+    /**
+     * 给group添加点击事件监听
+     * @param onGroupClickListener 监听
+     * @param isHeaderGroupClickable 表示header是否可点击<br/>
+     * note : 当不想group可点击的时候，需要在OnGroupClickListener#onGroupClick中返回true，
+     * 并将isHeaderGroupClickable设为false即可
+     */
+    public void setOnGroupClickListener(OnGroupClickListener onGroupClickListener, boolean isHeaderGroupClickable) {
+        mIsHeaderGroupClickable = isHeaderGroupClickable;
+        super.setOnGroupClickListener(onGroupClickListener);
+    }
+
+    //入口，设置固定头部刷新监听
+    public void setOnHeaderUpdateListener(OnHeaderUpdateListener listener) {
+        if (listener == null) {
+            mHeaderView = null;
+            mHeaderWidth = mHeaderHeight = 0;
+            return;
+        }
+
+        mHeaderUpdateListener = listener;
+        mHeaderView = listener.getPinnedHeader();
+        int firstVisiblePos = getFirstVisiblePosition();
+        int firstVisibleGroupPos = getPackedPositionGroup(getExpandableListPosition(firstVisiblePos));
+        listener.updatePinnedHeader(mHeaderView, firstVisibleGroupPos);
+        requestLayout();//重新测量，布局，绘制整个流程
+        postInvalidate();//只重新绘制流程，并且在子线程中调用，invalidate是在主线程中被调用
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //widthMeasureSpec heightMeasureSpec都是父容器的宽高
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (mHeaderView == null) {
+            return;
+        }
+        measureChild(mHeaderView, widthMeasureSpec, heightMeasureSpec);//measureChild是测量子View的宽高，measureChildren是用来测量所有子View的宽高
+        //测试之后就可以获取子view的宽高
+        mHeaderWidth = mHeaderView.getMeasuredWidth();
+        mHeaderHeight = mHeaderView.getMeasuredHeight();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if (mHeaderView == null) {
+            return;
+        }
+        int delta = mHeaderView.getTop();
+        mHeaderView.layout(0, delta, mHeaderWidth, mHeaderHeight + delta);//左上角x,y坐标，右下角x,y坐标
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) { //dispatchDraw是测绘制子view时候被调用，当绘制父容器是由ondraw()-> dispatchDraw()
+        super.dispatchDraw(canvas);
+        if (mHeaderView != null) {
+            drawChild(canvas, mHeaderView, getDrawingTime());
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+        int pos = pointToPosition(x, y);//获取被点击的item索引
+        if (mHeaderView != null && y >= mHeaderView.getTop() && y <= mHeaderView.getBottom()) { //被点击的是头部区域
+            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+                mTouchTarget = getTouchTarget(mHeaderView, x, y);
+                mActionDownHappened = true;
+            } else if (ev.getAction() == MotionEvent.ACTION_UP) {
+                View touchTarget = getTouchTarget(mHeaderView, x, y);
+                if (touchTarget == mTouchTarget && mTouchTarget.isClickable()) {
+                    mTouchTarget.performClick();
+                    invalidate(new Rect(0, 0, mHeaderWidth, mHeaderHeight));//实现局部刷新
+                } else if (mIsHeaderGroupClickable){
+                    int groupPosition = getPackedPositionGroup(getExpandableListPosition(pos));
+                    if (groupPosition != INVALID_POSITION && mActionDownHappened) {
+                        if (isGroupExpanded(groupPosition)) {
+                            collapseGroup(groupPosition);
+                        } else {
+                            expandGroup(groupPosition);
+                        }
+                    }
+                }
+                mActionDownHappened = false;
+            }
+            return true;
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private View getTouchTarget(View view, int x, int y) {
+        if (!(view instanceof ViewGroup)) {
+            return view;
+        }
+
+        ViewGroup parent = (ViewGroup)view;
+        int childrenCount = parent.getChildCount();
+        final boolean customOrder = isChildrenDrawingOrderEnabled();//子view是否是按照顺序进行绘制的
+        View target = null;
+        for (int i = childrenCount - 1; i >= 0; i--) {
+            final int childIndex = customOrder ? getChildDrawingOrder(childrenCount, i) : i;
+            final View child = parent.getChildAt(childIndex);
+            if (isTouchPointInView(child, x, y)) {
+                target = child;
+                break;
+            }
+        }
+        if (target == null) {
+            target = parent;
+        }
+
+        return target;
+    }
+
+    private boolean isTouchPointInView(View view, int x, int y) {
+        if (view.isClickable() && y >= view.getTop() && y <= view.getBottom()
+                && x >= view.getLeft() && x <= view.getRight()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void requestRefreshHeader() {
+        refreshHeader();
+        invalidate(new Rect(0, 0, mHeaderWidth, mHeaderHeight));
+    }
+
+    protected void refreshHeader() {
+        if (mHeaderView == null) {
+            return;
+        }
+        int firstVisiblePos = getFirstVisiblePosition();
+        int firstVisibleGroupPos = getPackedPositionGroup(getExpandableListPosition(firstVisiblePos));
+
+        int pos = firstVisiblePos + 1;
+        int group = getPackedPositionGroup(getExpandableListPosition(pos));
+
+        if (group == firstVisibleGroupPos + 1) { //当每组的group布局位于屏幕顶部时，此条件才会成立
+            View view = getChildAt(1);//获取index = 1的item （第一组group的第一个child）
+            if (view == null) {
+                return;
+            }
+
+            if (view.getTop() <= mHeaderHeight) { //第一个group部分页面盖住了屏幕顶部
+                int delta = mHeaderHeight - view.getTop();
+                mHeaderView.layout(0, -delta, mHeaderWidth, mHeaderHeight - delta);
+            } else {
+                //TODO : note it, when cause bug, remove it
+                mHeaderView.layout(0, 0, mHeaderWidth, mHeaderHeight);
+            }
+        } else {
+            //悬浮group布局在屏幕顶部，当每组的group布局位于屏幕其他位置（非顶部时）
+            mHeaderView.layout(0, 0, mHeaderWidth, mHeaderHeight);
+        }
+
+        if (mHeaderUpdateListener != null) {
+            mHeaderUpdateListener.updatePinnedHeader(mHeaderView, firstVisibleGroupPos);
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (mScrollListener != null) {
+            mScrollListener.onScrollStateChanged(view, scrollState);
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
+        if (totalItemCount > 0) {
+            refreshHeader();
+        }
+        if (mScrollListener != null) {
+            mScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+        }
+    }
+
+}
